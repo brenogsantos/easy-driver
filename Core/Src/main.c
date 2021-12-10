14b57c95a5265fee8f31491ef6a37788c6f7a835
Breno Gomes <brenog.santos41@gmail.com>
2021-12-10 20:09:20 -0300

    adição toggle gpio

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -10,7 +10,7 @@ int main(void)
 	easyGPIOConfig(GPIOA, PIN_1, OUTPUT);
 	easyGPIOConfigFullSet(GPIOE, PIN_3,INPUT, PULLUP, OUT_PUSH_PULL, OUT_FREQ_LOW);
 
-
+	easyGPIOSetState(GPIOA, 1, SET);
   while (1)
   {
 	  uint32_t j = 0;
@@ -18,11 +18,9 @@ int main(void)
 		for(int i = 0; i < 1000; i ++){
 			j++;
 		}
-	  if(GPIOE->IDR>>3 == 1){
-		 easyGPIOSetState(GPIOA, PIN_1, SET);
+	  if(GPIOE->IDR>>3 == 0){
+		 easyGPIOToggle(GPIOA, PIN_1);
 	  }
-	  else
-		  easyGPIOSetState(GPIOA, PIN_1, RESET);
 
   }
 
